0e94791da42347ca1ea6e162a957eb70aab5ac83
David Diaz <david.diaz@synapse.com>
2020-12-15 15:43:33 -0300

    Added by Jenkins to releaseQuince for auto export

---
+++ ejecutar proceso/ejecutarProceso.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /run %1 /user %2 %3 /dbconname %4
---
+++ release 1/processes/processdeploy.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "C:\Users\david.diaz\Desktop\BPA Process - First Process.xml" /overwrite /user %1 %2 /dbconname %3
